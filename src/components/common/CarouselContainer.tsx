c424981101ca675f69cffab6c695e817f7aa8be9
David Betancur <dbetancur@celerik.com>
2025-06-22 19:00:56 -0500

    Update CarouselContainer only show arrows when is required

--- src/components/common/CarouselContainer.tsx
+++ src/components/common/CarouselContainer.tsx
@@ -1,28 +1,51 @@
-import React, { useRef } from 'react';
-import { IconButton } from '@material-tailwind/react';
+import React, { useEffect, useRef, useState } from 'react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
+import { IconButton } from '@material-tailwind/react';
 
 type direction = 'left' | 'right';
 
 interface CarouselContainerProps {
+  alwaysShowArrows?: boolean;
   children: React.ReactNode;
 }
 
-const CarouselContainer: React.FC<CarouselContainerProps> = ({ children }) => {
+const CarouselContainer: React.FC<CarouselContainerProps> = ({
+  alwaysShowArrows = false,
+  children,
+}) => {
+  const [showArrows, setShowArrows] = useState<boolean>(alwaysShowArrows);
+
   const containerRef = useRef<HTMLDivElement>(null);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const checkOverflow = () => {
+      if (!alwaysShowArrows && containerRef.current && contentRef.current) {
+        setShowArrows(
+          contentRef.current.scrollWidth > containerRef.current.clientWidth,
+        );
+      }
+    };
+
+    checkOverflow();
+
+    window.addEventListener('resize', checkOverflow);
+    return () => window.removeEventListener('resize', checkOverflow);
+  }, [children]);
 
   const scroll = (direction: direction) => {
-    if (!containerRef.current) return;
-    const { scrollLeft, clientWidth } = containerRef.current;
+    if (!contentRef.current) return;
+
+    const { scrollLeft, clientWidth } = contentRef.current;
     const scrollAmount = clientWidth * 0.8;
 
     if (direction === 'left') {
-      containerRef.current.scrollTo({
+      contentRef.current.scrollTo({
         behavior: 'smooth',
         left: scrollLeft - scrollAmount,
       });
     } else {
-      containerRef.current.scrollTo({
+      contentRef.current.scrollTo({
         behavior: 'smooth',
         left: scrollLeft + scrollAmount,
       });
@@ -30,27 +53,31 @@ const CarouselContainer: React.FC<CarouselContainerProps> = ({ children }) => {
   };
 
   return (
-    <div className="w-full flex items-center gap-4">
-      <IconButton
-        className="p-4 bg-primary dark:bg-dk_primary"
-        onClick={() => scroll('left')}
-      >
-        <ChevronLeft className="h-6 w-6 text-white" />
-      </IconButton>
+    <div className="w-full flex items-center gap-4" ref={containerRef}>
+      {showArrows && (
+        <IconButton
+          className="p-4 bg-primary dark:bg-dk_primary"
+          onClick={() => scroll('left')}
+        >
+          <ChevronLeft className="h-6 w-6 text-white" />
+        </IconButton>
+      )}
 
       <div
-        ref={containerRef}
         className="flex gap-4 overflow-x-auto overflow-y-hidden scroll-smooth pb-2 w-full snap-x snap-mandatory"
+        ref={contentRef}
       >
         {children}
       </div>
 
-      <IconButton
-        className="p-4 bg-primary dark:bg-dk_primary"
-        onClick={() => scroll('right')}
-      >
-        <ChevronRight className="h-6 w-6 text-white" />
-      </IconButton>
+      {showArrows && (
+        <IconButton
+          className="p-4 bg-primary dark:bg-dk_primary"
+          onClick={() => scroll('right')}
+        >
+          <ChevronRight className="h-6 w-6 text-white" />
+        </IconButton>
+      )}
     </div>
   );
 };
